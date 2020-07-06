c525d125282f201acdcb335b8203331165dc0232
CTingCTer <gmiller290488@gmail.com>
2020-07-06 16:05:49 +0100

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Spare on 06/07/2020.
+//  Copyright © 2020 Spare. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
