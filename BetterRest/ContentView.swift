cce3a154feebaa327357d21c3befbc4165be0c2d
CTingCTer <gmiller290488@gmail.com>
2020-08-06 16:23:36 +0100

    App working

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,8 +9,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = defaultWakeTime
+    @State private var coffeeAmount = 1
+    
+    private var wakeupTime: String {
+        let model = SleepCalculator()
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try
+                model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            return formatter.string(from: sleepTime)
+        } catch {
+            return "Sorry, there was a problem calculating your bedtime."
+        }
+    }
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Form {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                   
+                    DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                    
+                    Section(header: Text("Desired amount of sleep")) {
+                        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                                           Text("\(sleepAmount, specifier: "%g") hours")
+                        }
+                    }
+                    
+                    Section(header: Text("Daily coffee intake")) {
+                        Picker(selection: $coffeeAmount, label: Text("\(coffeeAmount)")) {
+                            ForEach(1 ..< 20) {
+                                if $0 == 1 {
+                                    Text("1 cup")
+                                } else {
+                                    Text("\($0) cups")
+                                }
+                            }
+                        }
+                    .labelsHidden()
+                    }
+                }
+                .navigationBarTitle("BetterRest")
+                VStack(alignment: .center, spacing: 0) {
+                    Spacer()
+                    Text("You should go to bed at: ")
+                        .font(.headline)
+                    Text("\(wakeupTime)")
+                        .font(.headline)
+                        .foregroundColor(.red)
+                    Spacer()
+                }
+                
+            }
+        }
     }
 }
 
